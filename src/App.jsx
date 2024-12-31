a820dd2ee3d65aab8acceead9ce4c72d376145a7
Abk-coder <dkhadre@ept.sn>
2024-12-31 23:08:45 +0000

    refactoring css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
-import React, { useState } from 'react';
-import ProductList from './ProductList';
-import './App.css';
-import AboutUs from './AboutUs';
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import ProductList from "./ProductList";
+import "./App.css";
+import AboutUs from "./AboutUs";
 
 function App() {
-  
   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStartedClick = () => {
@@ -14,25 +13,32 @@ function App() {
 
   return (
     <div className="app-container">
-      <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
+      <div className={`landing-page ${showProductList ? "fade-out" : ""}`}>
         <div className="background-image"></div>
         <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
+          <div className="landing_content">
+            <h1>Welcome To Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Green Meets Serenity</p>
 
+            <button
+              className="get-started-button"
+              onClick={handleGetStartedClick}
+            >
+              Get Started
+            </button>
+          </div>
+          <div
+            className="aboutus_container"
+            style={{ width: "100%", marginRight: "20px" }}
+          >
+            <AboutUs />
+          </div>
+        </div>
       </div>
-      <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
+      <div
+        className={`product-list-container ${showProductList ? "visible" : ""}`}
+      >
         <ProductList />
       </div>
     </div>
@@ -40,6 +46,3 @@ function App() {
 }
 
 export default App;
-
-
-
